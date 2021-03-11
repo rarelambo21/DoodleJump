0cc2656800a8e741228e01c442eed944807ab159
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-11 17:43:23 +0300

    Doodle Jump

--- DoodleJump/DoodleJump.cpp
+++ DoodleJump/DoodleJump.cpp
@@ -1,20 +1,98 @@
 // DoodleJump.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace sf;
+
+struct point {
+	int x, y;
+};
+
+
+int main(){
+	srand(time(0));
+
+	RenderWindow app(VideoMode(400, 533), "Doodle Game!");
+	app.setFramerateLimit(144);
+
+	Texture t1, t2, t3;
+	t1.loadFromFile("background.png");
+	t2.loadFromFile("platform.png");
+	t3.loadFromFile("doodle.png");
+
+	Sprite sBackground(t1), sPlat(t2), sPers(t3);
+
+	point plat[20];
+
+	for (int i = 0; i < 10; i++) {
+		plat[i].x = rand() % 400;
+		plat[i].y = rand() % 533;
+	}
+
+	int x = 100, y = 100, h = 200;
+	float dx = 0, dy = 0;
+
+	while (app.isOpen()) {
+		Event e;
+
+		while (app.pollEvent(e)) {
+			if (e.type == Event::Closed)
+				app.close();
+		}
+
+		// Game Controls 
+		// ESC - End game
+		// A - Left
+		// D - Right
+		// Left Arrow - Left
+		// Right Arrow - Right
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+			app.close();
+
+		if (Keyboard::isKeyPressed(Keyboard::D))
+			x += 3;
+		if (Keyboard::isKeyPressed(Keyboard::A))
+			x -= 3;
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+			x += 3;
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+			x -= 3;
+
+		dy += 0.2;
+		y += dy; 
+		if (y > 500)
+			dy = -10;
+
+		if (y < h)
+			for (int i = 0; i < 10; i++) {
+				y = h;
+				plat[i].y = plat[i].y - dy;
+				if (plat[i].y > 533)
+				{
+					plat[i].y = 0; plat[i].x = rand() % 400;
+				}
+			}
+
+		for (int i = 0; i < 10; i++)
+			if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x + 68)
+				&& (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0))
+				dy = -10;
+
+		sPers.setPosition(x, y);
+
+		app.draw(sBackground);
+		app.draw(sPers);
+
+		for (int i = 0; i < 10; i++) {
+			sPlat.setPosition(plat[i].x, plat[i].y);
+			app.draw(sPlat);
+		}
+
+		app.display();
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
